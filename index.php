677d8afb0c0c9e675b5d8e087e75d8c2937116f1
javo-castorena <111478369+javo-castorena@users.noreply.github.com>
2024-04-28 17:16:06 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css" type="text/css">
+    <title>CONECTAPRO</title>
+</head>
+<body>
+        <div class="header">
+                <!-- Aquí va el contenido de tu encabezado -->
+                
+                <img src="https://trabajosdiarios.com/storage/logow/ve/Cone/conecta-pro-360-1702672824.webp" alt="Logo del sitio">
+                <nav>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Acerca</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </nav>
+        </div>
+
+
+        <div class="contenedor">
+            <!-- Todo el contenido de tu sitio va aquí -->
+            <div class="video-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/9QxRjC7wSyU?si=7D3JRFtGjfq_KKcn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+            
+            <!-- Más contenido -->
+        </div>
+
+        <footer>
+            <div class="footer-content">
+                <p>Nombre del Curso: Conceptualización de servicios en la nube</p>
+                <p>Tu Nombre Completo: Fco. Javier Castorena Jara</p>
+                <p>Código: 31078688</p>
+                <p>Correo de Contacto: javier.castorena@udgvirtual.udg.mx</p>
+            </div>
+         </footer>
+    
+</body>
+</html>
